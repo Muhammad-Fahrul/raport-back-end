11737c745ecf99414123b1c78b44ef4d76b92f8c
Muhammad-Fahrul <fahruldev22@gmail.com>
2024-04-16 10:35:31 +0800

    __dirname fixing

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors(corsOption));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Routes
